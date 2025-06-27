d8bd78130b31964b15be2b9582463679d4658df0
Dylan B <burns5@otterbein.edu>
2025-06-26 23:00:43 -0400

    Testing account change

--- src/main/java/dev/dburns/budget_tracker/BudgetTrackerApplication.java
+++ src/main/java/dev/dburns/budget_tracker/BudgetTrackerApplication.java
@@ -3,11 +3,15 @@ package dev.dburns.budget_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class BudgetTrackerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(BudgetTrackerApplication.class, args);
-	}
 
+
+	}
 }
