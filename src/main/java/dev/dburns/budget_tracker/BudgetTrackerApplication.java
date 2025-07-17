1e00c98447ff8cf9ef4246b73e5954c11b041f69
DBurns21 <wolfdb21@gmail.com>
2025-07-17 11:45:15 -0400

    updated some test code to make sure I was able to update the database properly

--- src/main/java/dev/dburns/budget_tracker/BudgetTrackerApplication.java
+++ src/main/java/dev/dburns/budget_tracker/BudgetTrackerApplication.java
@@ -1,11 +1,17 @@
 package dev.dburns.budget_tracker;
 
 import dev.dburns.budget_tracker.model.Category;
+import dev.dburns.budget_tracker.model.Expense;
+import dev.dburns.budget_tracker.model.User;
 import dev.dburns.budget_tracker.repository.CategoryRepository;
+import dev.dburns.budget_tracker.repository.ExpenseRepository;
+import dev.dburns.budget_tracker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
+
+import java.time.Instant;
 
 
 @SpringBootApplication
@@ -13,6 +19,10 @@ public class BudgetTrackerApplication implements  CommandLineRunner{
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	@Autowired
+	private ExpenseRepository expenseRepository;
+	@Autowired
+	private UserRepository userRepository;
 
 
 	public static void main(String[] args) {
@@ -21,14 +31,35 @@ public class BudgetTrackerApplication implements  CommandLineRunner{
 
 	@Override
 	public void run(String... args) {
+		User user = new User();
+		user.setEmail("example@example.com");
+		user.setName("Bill");
+		user.setPassword("password123");
+
+
+
 		Category cat = new Category();
 		cat.setName("Grocery");
 
+		userRepository.save(user);
 		categoryRepository.save(cat); // INSERT into database
 
+		Expense expense = new Expense();
+		expense.setDescription("Will probably change the tables later");
+		expense.setExpenseDate(Instant.now());
+
+		expenseRepository.save(expense);
+
+
 		System.out.println("All categories in the database:");
+		for (User printUser : userRepository.findAll()) {
+			System.out.println(printUser);
+		}
 		for (Category category : categoryRepository.findAll()) {
 			System.out.println(category);
+		}
+		for (Expense printExpense : expenseRepository.findAll()) {
+			System.out.println(printExpense);
 		}
 	}
 }
